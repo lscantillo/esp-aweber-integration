58084f0f8e67beae7259ecc0c18d6a5348fdc7f4
lscantillo <luissebastiancantillo@gmail.com>
2020-12-05 21:14:20 -0500

    User scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence:true, uniqueness: true
   validates :phone, presence: true
-  validates :receive_mail
 
-  
+  scope :by_receive_mail, ->(arg) { where(receive_mail: arg) }
+  scope :by_info_status_empty, -> { where(send_information_status: '') }
+  scope :by_info_status_not_empty, -> { where("send_information_status != ''") }
 end
