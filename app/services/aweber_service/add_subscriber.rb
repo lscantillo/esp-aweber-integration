b8bbd5a795a2b2d54d4b42508c4e698236a87185
lscantillo <luissebastiancantillo@gmail.com>
2020-12-06 12:16:34 -0500

    Add ENV Variables

--- app/services/aweber_service/add_subscriber.rb
+++ app/services/aweber_service/add_subscriber.rb
@@ -16,32 +16,17 @@ module AweberService
       token = AweberService::GetAccessToken.new().call
       @authorization = "Bearer #{token}"
       uri = URI( @uri + '/1.0/accounts/1663494/lists/5873413/subscribers')
-      body = json_data(@data['name'],@data['email'],@data['phone'],@data['receive_mail'],@data['send_information_status'])
-      response = HTTP.headers(:accept => "application/json", :Authorization => @authorization).post(uri,:json=> {
-        "email" => @data['email'],
-        "name" => @data['name'],
-        "custom_fields" => {
-            "Telefono" => @data['phone'],
-            "Deseas recibir correos de nosotros" => @data['receive_mail'],
-            "send-information-status" => @data['send_information_status'],
-        },
-        "update_existing": "true"
-      })
-     
-      puts "******BODY*******"
-      puts body
-      puts "******RESPONSE*******"
-      puts response
+      body = json_data(@data['email'],@data['name'],@data['phone'],@data['receive_mail'])
+      response = HTTP.headers(:accept => "application/json", "Content-Type" => "application/json", :Authorization => @authorization).post(uri,:json=> body)
+
     end  
 
     private
 
     def set_params
-      @uri = 'https://auth.aweber.com'
-      @client_id = 'TO8sYTwXOeVK964wtbRPNnAxOlfiLxWs'
-      @client_secret = 'sxF5SBmgFABUGTbMkLCEcnnX3xwe1OgU'
-      @grant_type = 'refresh_token'
-      @refresh_token = 'RKqFc99WgHXlSGNBFYmWV6uj53q3FuDz'  
+      @uri = ENV['URI_API']
+      @client_id = ENV['CLIENT_ID']
+      @client_secret = ENV['CLIENT_SECRET']
     end
 
     def json_data(email,name,phone,receive_mail,send_info)
@@ -52,11 +37,11 @@ module AweberService
               "custom_fields" => {
                   "Telefono" => phone,
                   "Deseas recibir correos de nosotros" => receive_mail,
-                  "send-information-status" => send_info,
+                  "send-information-status" => 'opted-in',
               },
               "update_existing": "true"
           }
-      data_user.to_json
+      data_user
     end
     
   end
--- app/services/user_service/search_new_subscriber.rb
+++ app/services/user_service/search_new_subscriber.rb
@@ -7,7 +7,11 @@ module UserService
     end
 
     def call
-      @new_users = User.by_receive_mail(true).by_info_status_empty.count
+      @new_users = User.by_receive_mail(true).by_info_status_empty
+      @new_users.find_each do |user|
+        AweberService::AddSubscriber.new(user).call
+      end
+      
       UserService::ChangeUserInfo.new().call
     end
 
